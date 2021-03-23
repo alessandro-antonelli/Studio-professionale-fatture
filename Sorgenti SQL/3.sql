badb5553cffa9acc8282fd7eebabc08a0c8841ca
alessandro-antonelli <alessandroantonelli2@gmail.com>
2021-03-23 16:33:50 +0100

    correzione group by

--- Sorgenti SQL/3.sql
+++ Sorgenti SQL/3.sql
@@ -2,5 +2,5 @@ SELECT	 P.CodiceFiscale, COUNT(F.IdFatture) as NumeroFatture
 FROM	 Persone as P JOIN Fatture as F
 		 ON F.IDpersonaIntestataria = P.IDpersona
 WHERE	 F.Importo > 1000
-GROUP BY P.IDpersone P.CodiceFiscale
+GROUP BY P.CodiceFiscale
 HAVING	 NumeroFatture > 3
